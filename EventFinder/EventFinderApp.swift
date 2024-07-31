7f57881d5c0b3764f01265bcdfdda35720e52b31
mshayan-experlabs <138456600+mshayan-experlabs@users.noreply.github.com>
2024-07-31 12:47:57 +0500

    Initial Commit

---
+++ EventFinder/EventFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventFinderApp.swift
+//  EventFinder
+//
+//  Created by Muhammad Shayan on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct EventFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
